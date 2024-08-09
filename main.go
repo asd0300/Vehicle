b773dfbaab9d8db9d8076f907572c0c7f1770035
Sol_Ben_Fan <ben.fan@solventosoft.com.tw>
2024-08-09 22:02:34 +0800

    add 2 user methed register and login

--- main.go
+++ main.go
@@ -5,14 +5,30 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
+
+var jwtKey []byte
+
+func init() {
+	// 加载 .env 文件
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	jwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+}
 
 type User struct {
 	Username  string    `json:"username" binding:"required"`
@@ -28,8 +44,48 @@ func main() {
 	client, _ = mongo.Connect(context.TODO(), clientOption)
 	defer client.Disconnect(context.TODO())
 
+	r.POST("/login", loginUser)
 	r.POST("/register", registerUser)
-	r.Run(":4000")s
+	r.Run(":4000")
+}
+
+type LoginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func loginUser(c *gin.Context) {
+	var loginRequest LoginRequest
+	if err := c.ShouldBindJSON(&loginRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user User
+	collection := client.Database("car-repair-system").Collection("users")
+	err := collection.FindOne(context.TODO(), bson.M{"username": loginRequest.Username}).Decode(&user)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	// Compare passwords
+	hash := sha256.New()
+	hash.Write([]byte(loginRequest.Password))
+	if user.Password != hex.EncodeToString(hash.Sum(nil)) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	// Create JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+		"role":     user.Role,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, _ := token.SignedString(jwtKey)
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
 
 func registerUser(c *gin.Context) {
