0088408ac99951755a752b9150a3b03b28764962
Sol_Ben_Fan <ben.fan@solventosoft.com.tw>
2024-08-13 14:05:41 +0800

    新增mongodb 存在判斷 新增 jwt middleware 新增 appointment 新增smtp confirmation email

--- database/mongodb.go
+++ database/mongodb.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,6 +19,20 @@ func init() {
 
 func CreateMongoConnect() {
 	clientOption := options.Client().ApplyURI("mongodb://localhost:27017")
-	Client, _ = mongo.Connect(context.TODO(), clientOption)
+	Client, err := mongo.Connect(context.TODO(), clientOption)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	// 設定 ping 的超時時間為 5 秒
+	ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Second)
+	defer cancel()
+
+	err = Client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Failed to ping MongoDB:", err)
+	} else {
+		fmt.Println("Successfully connected to MongoDB!")
+	}
 	AppointmentCollection = Client.Database("car-repair-system").Collection("appointments")
 }
